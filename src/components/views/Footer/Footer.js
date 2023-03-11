3f2acfcb039466183dd8de6a175fd258cb98897b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-11 12:18:00 +0100

    Add Footer component

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,5 +1,17 @@
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Row className=' text-muted justify-content-center'>
+        <Col className='mb-4' md='auto'>
+          Waiter-app © 2023
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
