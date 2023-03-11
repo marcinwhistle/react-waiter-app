ad03a8342b6ac3bb1c8c4233ebddaf00489d38c5
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-11 11:45:11 +0100

    Add React Router routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Container>
+      <Header>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/tables/:id' element={<Table />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </Header>
+    </Container>
+  );
 }
 
 export default App;
