33ae09ff1ea32501d0811dd047fa991e62434d4b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-11 12:07:54 +0100

    Add NavBar component

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,5 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Navbar
+      bg='primary'
+      variant='dark'
+      expand='lg'
+      className='mt-4 mb-4 rounded'
+    >
+      <Container>
+        <Navbar.Brand href='/'>Waiter-app</Navbar.Brand>
+        <Nav>
+          <Nav.Link as={NavLink} to='/'>
+            Home
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
