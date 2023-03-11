ca01bba509a9533b4f8304a0aa5bf6714fa5f7db
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-03-11 11:51:03 +0100

    Add basic components structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import NotFound from './components/pages/NotFound/NotFound';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import Table from './components/features/Table/Table';
 
 function App() {
   return (
     <Container>
-      <Header>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/tables/:id' element={<Table />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
-      </Header>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/tables/:id' element={<Table />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
     </Container>
   );
 }
