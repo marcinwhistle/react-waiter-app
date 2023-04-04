a2d8cb8e837022ad34cfcb6702bf8daf85542ae7
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-04-04 11:45:50 +0200

    Add EditTableForm component with functionality

--- src/components/features/EditTableForm/EditTableForm.js
+++ src/components/features/EditTableForm/EditTableForm.js
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { getTableById } from '../../../redux/tablesRedux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTableById, editTableRequest } from '../../../redux/tablesRedux';
 import { Form } from 'react-bootstrap';
 import { useState } from 'react';
 import { Button } from 'react-bootstrap';
-
+import { Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 const EditTableForm = () => {
   const { id } = useParams();
   const tableData = useSelector((state) => getTableById(state, id));
@@ -15,8 +16,16 @@ const EditTableForm = () => {
     tableData.maxPeopleAmount
   );
   const [bill, setBill] = useState(tableData.bill);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      editTableRequest({ status, peopleAmount, maxPeopleAmount, bill, id })
+    );
+    navigate('/');
+  };
 
   if (!tableData) {
     return <Navigate to='/' />;
@@ -24,32 +33,40 @@ const EditTableForm = () => {
   return (
     <>
       <Form onSubmit={handleSubmit}>
-        <Form.Group className='d-flex align-items-center w-25'>
+        <Form.Group className='d-flex align-items-center w-25 mb-3'>
           <Form.Label className='mx-2'>Status:</Form.Label>
           <Form.Select onChange={(e) => setStatus(e.target.value)}>
             <option>{status}</option>
           </Form.Select>
         </Form.Group>
-        <Form.Group className='d-flex align-items-center w-25'>
+        <Form.Group className='d-flex align-items-center w-25 mb-3'>
           <Form.Label className='mx-2'>People:</Form.Label>
-          <Form.Control
-            value={peopleAmount}
-            onChange={(e) => setPeopleAmount(e.target.value)}
-          ></Form.Control>{' '}
-          /
-          <Form.Control
-            value={maxPeopleAmount}
-            onChange={(e) => setMaxPeopleAmount(e.target.value)}
-          ></Form.Control>
+          <Col>
+            <Form.Control
+              value={peopleAmount}
+              onChange={(e) => setPeopleAmount(e.target.value)}
+            ></Form.Control>{' '}
+          </Col>
+          <span className='mx-2'>/</span>
+          <Col>
+            <Form.Control
+              value={maxPeopleAmount}
+              onChange={(e) => setMaxPeopleAmount(e.target.value)}
+            />
+          </Col>
         </Form.Group>
-        <Form.Group className='d-flex align-items-center w-25'>
+        <Form.Group className='d-flex align-items-center w-25 mb-3'>
           <Form.Label className='mx-2'>Bill:</Form.Label>
-          <Form.Control
-            value={bill}
-            onChange={(e) => setBill(e.target.value)}
-          ></Form.Control>
+          <Col sm={4} className='mx-4'>
+            <Form.Control
+              value={bill}
+              onChange={(e) => setBill(e.target.value)}
+            />
+          </Col>
         </Form.Group>
-        <Button type='submit'>Update</Button>
+        <Button className='mx-2' type='submit'>
+          Update
+        </Button>
       </Form>
     </>
   );
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -6,9 +6,10 @@ export const getTableById = ({ tables }, id) => {
 //actions
 const createActionName = (actionName) => `app/tables.${actionName}`;
 const UPDATE_TABLES = createActionName('UPDATE_TABLES');
-
+const EDIT_TABLE = createActionName('EDIT_TABLE');
 //action creators
 export const updateTables = (payload) => ({ type: UPDATE_TABLES, payload });
+export const editTable = (payload) => ({ type: EDIT_TABLE, payload });
 export const fetchTables = () => {
   return (dispatch) => {
     fetch('http://localhost:3131/api/tables')
@@ -17,10 +18,35 @@ export const fetchTables = () => {
   };
 };
 
+export const editTableRequest = (editData) => {
+  return (dispatch) => {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: editData.id,
+        status: editData.status,
+        peopleAmount: editData.peopleAmount,
+        maxPeopleAmount: editData.maxPeopleAmount,
+        bill: editData.bill,
+      }),
+    };
+    fetch('http://localhost:3131/api/tables/' + editData.id, options).then(() =>
+      dispatch(editTable(editData))
+    );
+  };
+};
+
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
     case UPDATE_TABLES:
       return [...action.payload];
+    case EDIT_TABLE:
+      return [
+        statePart.map((table) => (table.id === action.id ? action : table)),
+      ];
     default:
       return statePart;
   }
