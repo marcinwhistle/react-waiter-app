f5eb95eb9749f57bf2930ac727665578b6bf26bb
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-04-06 08:04:30 +0200

    Fix config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ?  '/api' : 'http://localhost:3131/api'’
+export const API_URL =
+  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3131/api';
