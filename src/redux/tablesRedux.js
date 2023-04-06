f545455695c68789f55850ee07fdb1871d60e07a
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-04-06 08:15:51 +0200

    Fix tablesRedux

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -13,7 +13,7 @@ export const updateTables = (payload) => ({ type: UPDATE_TABLES, payload });
 export const editTable = (payload) => ({ type: EDIT_TABLE, payload });
 export const fetchTables = () => {
   return (dispatch) => {
-    fetch(API_URL)
+    fetch(API_URL + '/tables')
       .then((res) => res.json())
       .then((tables) => dispatch(updateTables(tables)));
   };
